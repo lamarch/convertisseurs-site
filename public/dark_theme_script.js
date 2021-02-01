f6b13bb8dcc91b0eb0001e7479aab24830055dd9
lamarch <paulmarchal54370@gmail.com>
2021-02-01 20:42:10 +0100

    Ajout d'un localStorage afin de retenir les préférences utilisateurs du thème

--- public/dark_theme_script.js
+++ public/dark_theme_script.js
@@ -4,7 +4,15 @@ const root = $(':root')
 input.on('click', () => {
     if (input.is(':checked')) {
         root.addClass('dark-theme')
+        window.localStorage.setItem('theme', 'dark')
     } else {
         root.removeClass('dark-theme')
+        window.localStorage.setItem('theme', '')
     }
 })
+
+const theme = window.localStorage.getItem('theme')
+if (theme === 'dark') {
+    input.prop('checked', false)
+    input.trigger('click')
+}
